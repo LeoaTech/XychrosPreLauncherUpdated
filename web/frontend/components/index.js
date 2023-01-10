87eeb1516553b6f14e1a96d4c9b8b32f7f002155
razaakomal <komal.raza@leoatech.com>
2023-01-10 09:58:35 +0500

    creat updated homepage

---
+++ web/frontend/components/index.js
@@ -0,0 +1,8 @@
+export { ProductsCard } from "./ProductsCard";
+export { default as SideBar } from "./sidebar/SideBar";
+export { default as Header } from "./header/Header";
+export { default as HomeComponent } from "./home/HomeComponent";
+//export { default as Campaign } from "./campaigns/Campaign";
+//export { default as Referral } from "./referrals/Referrals";
+export { default as MainPage } from "./ui/MainPage";
+export * from "./providers";
