3cdc5bc903b13a0d3415256b28978901ef834851
razaakomal <komal.raza@leoatech.com>
2023-01-19 18:36:57 +0500

    Update HomePage Page

--- web/frontend/pages/home.jsx
+++ web/frontend/pages/home.jsx
@@ -13,18 +13,18 @@ export default function Home() {
         <SideBar />
       </div>
     ) : (
-      <div className={darkTheme ?"sidebar":"sidebar dark"}>
+      <div className={darkTheme ?"sidebar closed":"sidebar dark"}>
         <SideBar />
       </div>
     )}
-   { activeMenu ? <div className={darkTheme ? "main__container dark":"main__container"}>
+   { activeMenu ? <div className={darkTheme ? "main__container ":"main__container dark"}>
       <MainPage>
         <div className="header">
           <Header />
         </div>
         <HomeComponent />
       </MainPage>
-    </div>:  <div className={darkTheme ?"main__container dark": "main__container"}>
+    </div>:  <div className={darkTheme ?"main__container full": "main__container dark"}>
       <MainPage>
         <div className="header">
           <Header />
