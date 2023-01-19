19836eb27432f03377cfc3866f466e644dc20b1f
razaakomal <komal.raza@leoatech.com>
2023-01-19 18:32:59 +0500

    Settings Form

--- web/frontend/pages/settings.jsx
+++ web/frontend/pages/settings.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import {
+  SideBar,
+  Header,
+  Settings,
+  MainPage,
+} from "../components/index";
+import { useStateContext } from "../contexts/ContextProvider";
+import { useThemeContext } from "../contexts/ThemeContext";
+import "../index.css";
 
-const Settings = () => {
+const SettingsPage = () => {
+  const { activeMenu } = useStateContext();
+  const { darkTheme } = useThemeContext();
   return (
-    <div>Settings</div>
-  )
-}
+    <div className="app">
+      {activeMenu ? (
+        <div className={darkTheme ? "sidebar" : "sidebar dark"}>
+          <SideBar />
+        </div>
+      ) : (
+        <div className={darkTheme ? "sidebar closed" : "sidebar dark"}>
+          <SideBar />
+        </div>
+      )}
+      {activeMenu ? (
+        <div className={darkTheme ? "main__container" : "main__container dark"}>
+          <MainPage>
+            <div className="header">
+              <Header />
+            </div>
+            <Settings />
+          </MainPage>
+        </div>
+      ) : (
+        <div className={darkTheme ? "main__container full" : "main__container dark"}>
+          <MainPage>
+            <div className="header">
+              <Header />
+            </div>
+            <Settings />
+          </MainPage>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Settings
+
+export default SettingsPage
