7acac323b9a9badc4a84f622050a9b36b2d78059
razaakomal <komal.raza@leoatech.com>
2023-01-19 18:34:47 +0500

    Update NewCampaign Page

--- web/frontend/pages/newcampaign.jsx
+++ web/frontend/pages/newcampaign.jsx
@@ -18,12 +18,12 @@ const NewCampaign = () => {
           <SideBar />
         </div>
       ) : (
-        <div className={darkTheme ? "sidebar" : "sidebar dark"}>
+        <div className={darkTheme ? "sidebar closed" : "sidebar dark"}>
           <SideBar />
         </div>
       )}
       {activeMenu ? (
-        <div className={darkTheme ? "main__container dark" : "main__container"}>
+        <div className={darkTheme ? "main__container " : "main__container dark"}>
           <MainPage>
             <div className="header">
               <Header />
@@ -32,7 +32,7 @@ const NewCampaign = () => {
           </MainPage>
         </div>
       ) : (
-        <div className={darkTheme ? "main__container dark" : "main__container"}>
+        <div className={darkTheme ? "main__container full" : "main__container dark"}>
           <MainPage>
             <div className="header">
               <Header />
