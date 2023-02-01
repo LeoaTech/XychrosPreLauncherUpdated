b108ec2ec24a683a1e66ba553a596e8a8974e976
razaakomal <komal.raza@leoatech.com>
2023-02-01 11:17:49 +0500

    Change Components import file

--- web/frontend/components/index.js
+++ web/frontend/components/index.js
@@ -5,9 +5,12 @@ export { default as HomeComponent } from "./home/HomeComponent";
 export { default as Campaign } from "./campaigns/Campaign";
 export { default as Referral } from "./referrals/Referrals";
 export { default as MainPage } from "./ui/MainPage";
-//export { default as UserProfile } from "./user/UserProfile";
-//export { default as Pricing } from "./pricing/Pricing";
-//export { default as FAQ } from "./faq/FAQ";
-export {default as NewCampaignForm} from "./newcampaign/NewCampaignForm";
-export {default as Settings} from "./settings/SettingComponent"
+export { default as Integrations } from "./integration/Integrations";
+export { default as UserProfile } from "./user/UserProfile";
+export { default as Pricing } from "./pricing/PriceComponent";
+export { default as FAQ } from "./faq/FAQ";
+export { default as NewCampaignForm } from "./newcampaign/NewCampaignForm";
+export { default as Settings } from "./settings/SettingComponent";
+export { default as Support } from "./support/SupportComponent";
+export { default as Feedback } from "./feedback/Feedback";
 export * from "./providers";
