cd5a4fbc542648e5926903e508e7dcbfbd238420
razaakomal <komal.raza@leoatech.com>
2023-02-01 11:10:39 +0500

    Add Social Links file

---
+++ web/frontend/components/newcampaign/socialLinks.jsx
@@ -0,0 +1,50 @@
+import { AiOutlineCalendar, AiFillInstagram } from "react-icons/ai";
+import { FaFacebook, FaWhatsappSquare, FaTiktok } from "react-icons/fa";
+import { TbBrandSnapchat } from "react-icons/tb";
+import { MdEmail } from "react-icons/md";
+import { BsTwitter } from "react-icons/bs";
+
+export const integratelinks = [
+  {
+    id: 1,
+    icon: <FaFacebook />,
+    title: "Facebook",
+    desc: "Share Referral Link via Facebook ",
+  },
+  {
+    id: 2,
+    icon: <AiFillInstagram />,
+    title: "Instagram",
+    desc: "Share Referral Link via Instagram ",
+  },
+  {
+    id: 3,
+    icon: <BsTwitter />,
+    title: "Twitter",
+    desc: "Share Referral Link via Twitter ",
+  },
+  {
+    id: 4,
+    icon: <FaTiktok />,
+    title: "TikTok",
+    desc: "Share Referral Link via TikTok ",
+  },
+  {
+    id: 5,
+    icon: <TbBrandSnapchat />,
+    title: "Snapchat",
+    desc: "Share Referral Link via Snapchat ",
+  },
+  {
+    id: 6,
+    icon: <FaWhatsappSquare />,
+    title: "Whatsapp",
+    desc: "Share Referral Link via Whatsapp ",
+  },
+  {
+    id: 7,
+    icon: <MdEmail />,
+    title: "Email",
+    desc: "Share Referral Link via Email ",
+  },
+];
