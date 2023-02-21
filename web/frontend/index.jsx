6996dd6350b5253e6d774345431f528d3b668fea
razaakomal <komal.raza@leoatech.com>
2023-02-21 11:17:30 +0500

    -

--- web/frontend/index.jsx
+++ web/frontend/index.jsx
@@ -3,13 +3,18 @@ import "./index.css";
 import App from "./App";
 import { ContextProvider } from "./contexts/ContextProvider";
 import { ThemeProvider } from "./contexts/ThemeContext";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
 
 ReactDOM.render(
   // Wrap the app inside the Context provider
-  <ThemeProvider>
-    <ContextProvider>
-      <App />
-    </ContextProvider>
-  </ThemeProvider>,
+  <Provider store={store}>
+    <ThemeProvider>
+      <ContextProvider>
+        <App />
+      </ContextProvider>
+    </ThemeProvider>
+  </Provider>,
+
   document.getElementById("app")
 );
