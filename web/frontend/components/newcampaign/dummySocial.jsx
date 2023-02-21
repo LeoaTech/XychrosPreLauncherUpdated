f54c0514f1c2f22a05eb35484ae2747a32b9bee5
razaakomal <komal.raza@leoatech.com>
2023-02-21 11:02:24 +0500

    Added New Campaign Basic form social links

---
+++ web/frontend/components/newcampaign/dummySocial.jsx
@@ -0,0 +1,34 @@
+import { AiOutlineCalendar, AiFillInstagram } from "react-icons/ai";
+import {
+  FaFacebook,
+  FaSnapchatGhost,
+  FaDiscord,
+  FaTiktok,
+} from "react-icons/fa";
+import { BsTwitter } from "react-icons/bs";
+export const storeLinks = [
+  {
+    id: 1,
+    icon: <FaFacebook />,
+  },
+  {
+    id: 2,
+    icon: <AiFillInstagram />,
+  },
+  {
+    id: 3,
+    icon: <BsTwitter />,
+  },
+  {
+    id: 4,
+    icon: <FaTiktok />,
+  },
+  {
+    id: 5,
+    icon: <FaSnapchatGhost />,
+  },
+  {
+    id: 6,
+    icon: <FaDiscord />,
+  },
+];
