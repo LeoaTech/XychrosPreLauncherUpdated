66cb97f6584adedc1564c623c59bceaa455f9297
razaakomal <komal.raza@leoatech.com>
2023-02-21 10:07:04 +0500

    Remove extra home page

--- web/frontend/pages/index.jsx
+++ web/frontend/pages/index.jsx
@@ -1,10 +1,32 @@
-import { SideBar, Header, HomeComponent, MainPage} from "../components/index";
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import {
+  fetchCampaign,
+  fetchAllCampaigns,
+} from "../app/features/campaigns/campaignSlice";
+import useFetchCampaignsData from "../app/features/constant/fetchCampaignsData";
+import { SideBar, Header, HomeComponent, MainPage } from "../components/index";
 import { useStateContext } from "../contexts/ContextProvider";
 import { useThemeContext } from "../contexts/ThemeContext";
 import "../index.css";
+import { useSelector } from "react-redux";
+
 export default function HomePage() {
-  const {activeMenu}  = useStateContext()
-  const {darkTheme,lightTheme} = useThemeContext()
+  const { activeMenu } = useStateContext();
+  const { darkTheme, lightTheme } = useThemeContext();
+  const dispatch = useDispatch();
+  let data = useFetchCampaignsData("/api/getcampaigns", {
+    method: "GET",
+    headers: { "Content-Type": "application/json" },
+  });
+  useEffect(() => {
+    if (data) {
+      dispatch(fetchCampaign(data));
+    }
+  }, [dispatch, data]);
+
+  
+
   return (
     <div className="app">
       {activeMenu ? (
@@ -16,21 +38,29 @@ export default function HomePage() {
           <SideBar />
         </div>
       )}
-     { activeMenu ? <div className={darkTheme ? "main__container":"main__container dark"}>
-        <MainPage className="sidebar-overlay">
-          <div className="header">
-            <Header />
-          </div>
-          <HomeComponent />
-        </MainPage>
-      </div>:  <div className={darkTheme ? "main__container full":"main__container dark"}>
-        <MainPage className="sidebar-overlay">
-          <div className="header">
-            <Header />
-          </div>
-          <HomeComponent />
-        </MainPage>
-      </div>}
+      {activeMenu ? (
+        <div className={darkTheme ? "main__container" : "main__container dark"}>
+          <MainPage className="sidebar-overlay">
+            <div className="header">
+              <Header />
+            </div>
+            <HomeComponent />
+          </MainPage>
+        </div>
+      ) : (
+        <div
+          className={
+            darkTheme ? "main__container full" : "main__container dark"
+          }
+        >
+          <MainPage className="sidebar-overlay">
+            <div className="header">
+              <Header />
+            </div>
+            <HomeComponent />
+          </MainPage>
+        </div>
+      )}
     </div>
   );
 }
