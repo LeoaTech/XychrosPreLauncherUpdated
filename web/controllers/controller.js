e801f2f5ad6e3078ec39da2b9550597002f677e2
leaoTech-hashim <muhammad.hashim@leoatech.com>
2023-03-16 14:06:24 +0500

    XYC-11 check whether entered email exists in the database or not.

---
+++ web/controllers/controller.js
@@ -0,0 +1,35 @@
+import { pool } from "../config/db.js";
+import { v4 as uuidv4 } from "uuid";
+import { Shopify, LATEST_API_VERSION } from "@shopify/shopify-api";
+import emailValidator from "deep-email-validator";
+import { sendemail } from "../utils/sendEmail.js";
+export const check_email = async (req, res, next) => {
+  try {
+    const { email, campaign_id } = req.body;
+    if (!email) {
+      return res
+        .status(400)
+        .json({ success: false, message: "Please provide valid input" });
+    }
+    let isemail_valid = await emailValidator.validate(email);
+    if (isemail_valid.validators.smtp.valid == false) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Please provide a valid email" });
+    }
+    let check_email = await pool.query(
+      "SELECT * FROM referrals WHERE email=$1",
+      [email]
+    );
+    if (check_email.rows.length > 0) {
+      return res
+        .status(200)
+        .json({ success: true, message: "You are logged in" });
+    } else {
+  } catch (error) {
+    console.log(error);
+    return res
+      .status(500)
+      .json({ success: false, message: "Something went wrong" });
+  }
+};
