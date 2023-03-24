b56920a0a836a0d4aadd3ca1c4b642c789315279
razaakomal <komal.raza@leoatech.com>
2023-03-24 13:26:55 +0500

    XYC-37 Update/Save Campaigns Settings

--- web/frontend/pages/campaigns/index.jsx
+++ web/frontend/pages/campaigns/index.jsx
@@ -1,40 +1,58 @@
-import React from 'react'
-import { SideBar, Header, Campaign, MainPage} from "../../components/index";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  fetchAllCampaigns,
+  fetchCampaignsData,
+  getCampaignsError,
+  getCampaignsStatus,
+} from "../../app/features/campaigns/campaignSlice";
+import { SideBar, Header, Campaign, MainPage } from "../../components/index";
 import { useStateContext } from "../../contexts/ContextProvider";
-import { useThemeContext } from '../../contexts/ThemeContext';
+import { useThemeContext } from "../../contexts/ThemeContext";
+import { useAuthenticatedFetch } from "../../hooks";
 
 const Campaigns = () => {
-  const {activeMenu}  = useStateContext();
-  const {darkTheme} = useThemeContext();
+  const { activeMenu } = useStateContext();
+  const { darkTheme } = useThemeContext();
+  const fetch = useAuthenticatedFetch();
+  const dispatch = useDispatch();
 
   return (
     <div className="app">
-    {activeMenu ? (
-      <div className={darkTheme ?"sidebar":"sidebar dark"}>
-        <SideBar />
-      </div>
-    ) : (
-      <div className={darkTheme ?"sidebar closed":"sidebar dark"}>
-        <SideBar />
-      </div>
-    )}
-   { activeMenu ? <div className={darkTheme ? "main__container":"main__container dark"}>
-      <MainPage>
-        <div className="header">
-          <Header />
+      {activeMenu ? (
+        <div className={darkTheme ? "sidebar" : "sidebar dark"}>
+          <SideBar />
         </div>
-        <Campaign />
-      </MainPage>
-    </div>:  <div className={darkTheme ?"main__container full": "main__container dark"}>
-      <MainPage>
-        <div className="header">
-          <Header />
+      ) : (
+        <div className={darkTheme ? "sidebar closed" : "sidebar dark"}>
+          <SideBar />
         </div>
-        <Campaign />
-      </MainPage>
-    </div>}
-  </div>
-  )
-}
+      )}
+      {activeMenu ? (
+        <div className={darkTheme ? "main__container" : "main__container dark"}>
+          <MainPage>
+            <div className="header">
+              <Header />
+            </div>
+            <Campaign />
+          </MainPage>
+        </div>
+      ) : (
+        <div
+          className={
+            darkTheme ? "main__container full" : "main__container dark"
+          }
+        >
+          <MainPage>
+            <div className="header">
+              <Header />
+            </div>
+            <Campaign />
+          </MainPage>
+        </div>
+      )}
+    </div>
+  );
+};
 
-export default Campaigns
+export default Campaigns;
