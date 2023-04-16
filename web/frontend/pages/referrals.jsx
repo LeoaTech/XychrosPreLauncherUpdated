d3158ae99eb7e0cc8eae427262a40fba4cdb38be
razaakomal <komal.raza@leoatech.com>
2023-04-16 07:21:49 +0500

    removed Sidebar flikering issue

--- web/frontend/pages/referrals.jsx
+++ web/frontend/pages/referrals.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { SideBar, Header, Referral, MainPage } from "../components/index";
 import { useStateContext } from "../contexts/ContextProvider";
 import { useThemeContext } from "../contexts/ThemeContext";
@@ -7,41 +7,42 @@ import "../index.css";
 const Referrals = () => {
   const { activeMenu } = useStateContext();
   const { darkTheme, lightTheme } = useThemeContext();
-
+ // Page render Scroll to Top
+ useEffect(()=>{
+  window.scrollTo(0, 0);
+},[])
   return (
     <div className="app">
       {activeMenu ? (
-        <div className={darkTheme ? "sidebar" : "sidebar dark"}>
-          <SideBar />
+        <div className="header">
+          <Header />
         </div>
       ) : (
-        <div className={darkTheme ? "sidebar closed" : "sidebar dark"}>
-          <SideBar />
+        <div className="header">
+          <Header />
         </div>
       )}
-      {activeMenu ? (
-        <div className={darkTheme ? "main__container" : "main__container dark"}>
-          <MainPage>
-            <div className="header">
-              <Header />
+      <div className="main-app">
+        {activeMenu ? (
+          <>
+            <div className="sidebar">
+              <SideBar />
             </div>
-            <Referral />
-          </MainPage>
-        </div>
-      ) : (
-        <div
-          className={
-            darkTheme ? "main__container full" : "main__container dark"
-          }
-        >
-          <MainPage>
-            <div className="header">
-              <Header />
+            <div className="main-container">
+              <Referral />
             </div>
-            <Referral />
-          </MainPage>
-        </div>
-      )}
+          </>
+        ) : (
+          <>
+            <div className="sidebar closed">
+              <SideBar />
+            </div>
+            <div className="main-container full">
+              <Referral />
+            </div>
+          </>
+        )}
+      </div>
     </div>
   );
 };
