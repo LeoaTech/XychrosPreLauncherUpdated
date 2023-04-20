59f0d209ce208f036fab655a72575603e75b7524
razaakomal <komal.raza@leoatech.com>
2023-04-20 12:34:16 +0500

    Move Shopify_assets files to asset folder

---
+++ web/frontend/assets/shopify_assets/index.js
@@ -0,0 +1,11 @@
+export { default as food } from "./food.png";
+export { default as food2 } from "./food2.png";
+export { default as jewelry } from "./jewelry.png";
+export { default as jewelry2 } from "./jewelry2.png";
+export { default as nature } from "./nature.png";
+export { default as nature2 } from "./nature2.png";
+export { default as WelcomeClothing } from "./WelcomeClothing.png";
+export { default as WelcomeFood } from "./WelcomeFood.png";
+export { default as WelcomeFood2 } from "./WelcomeFood2.png";
+export { default as WelcomeNature } from "./WelcomeNature.png";
+export { default as WelcomeJewelry } from "./WelcomeJewelry.png";
