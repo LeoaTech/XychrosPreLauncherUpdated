08b1c486beaff8f0bbb62f771204b068cab0c9be
leoaTech-hashim <125467835+leoaTech-hashim@users.noreply.github.com>
2023-05-15 22:51:13 -0700

    Add files via upload
    Signed-off-by: leoaTech-hashim <125467835+leoaTech-hashim@users.noreply.github.com>

---
+++ extensions/product-prelauncher/assets/first-page.js
@@ -0,0 +1,44 @@
+// var form = document.getElementById('form-submission'),
+//   actionPath = '',
+//   formData = null;
+
+// var xhr = new XMLHttpRequest();
+
+// form.addEventListener(
+//   'submit',
+//   (e) => {
+//     console.log('I came here');
+//     e.preventDefault();
+
+//     formData = new FormData(form);
+//     actionPath = form.getAttribute('action');
+
+//     console.log(formData);
+//     console.log(actionPath);
+
+//     xhr.open('GET', actionPath);
+//     xhr.send(formData);
+//   },
+//   false
+// );
+
+console.log("hello world");
+console.log(first_page_settings);
+
+const update_clicks = async () => {
+  const response = await fetch(
+    "/apps/xychros_backend_new/api/updatecampaigns/",
+    {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        campaign_name: "My Campaign 2903",
+      }),
+    }
+  );
+  const data = await response.json();
+  console.log(data);
+};
+update_clicks();
