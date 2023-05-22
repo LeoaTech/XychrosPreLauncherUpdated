38e4b507c3969d70b0ac7bde4eb34869e8a71030
razaakomal <komal.raza@leoatech.com>
2023-05-22 06:22:49 +0500

    Save the Pricing Plans Details in Database

---
+++ web/middleware/get-pricing-plans-api.js
@@ -0,0 +1,29 @@
+import { Shopify } from "@shopify/shopify-api";
+import NewPool from "pg";
+const { Pool } = NewPool;
+const pool = new Pool({
+    connectionString: "postgres://postgres:postgres@localhost:5432/prelauncher",
+});
+
+pool.connect((err, result) => {
+    if (err) throw err;
+    console.log("Connected");
+});
+
+export default function pricingPlansApiEndpoints(app) {
+    //read all Pricng plans
+    app.get('/api/pricing-plans', async (req, res) => {
+        try {
+            const price_plans = await pool.query(
+                `SELECT * FROM  pricing_plans`,
+            );
+
+            return res.status(200).json(price_plans.rows);
+
+        } catch (error) {
+            console.log(error)
+        }
+    })
+
+
+}
