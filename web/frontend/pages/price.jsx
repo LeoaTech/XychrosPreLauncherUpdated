38b2ee0ac4304e30179b6725764923788248fc55
razaakomal <komal.raza@leoatech.com>
2023-05-31 14:23:43 +0500

    XYC-161 Get Current Billing Plan

--- web/frontend/pages/price.jsx
+++ web/frontend/pages/price.jsx
@@ -5,6 +5,8 @@ import "../index.css";
 import useFetchPricingPlans from "../constant/fetchPricingPlans";
 import { fetchpricing } from "../app/features/pricing/pricing";
 import { useDispatch } from "react-redux";
+import useFetchCurrentPlan from "../constant/fetchCurrentPlan";
+import { UpdateCurrentPlan, fetchSavePlan } from "../app/features/current_plan/current_plan";
 
 const PricePage = () => {
   const { activeMenu } = useStateContext();
@@ -19,13 +21,38 @@ const PricePage = () => {
       "Content-Type": "application/json",
     },
   });
-  console.log(response)
+  // Get Current Plan Active 
+  const response2 = useFetchCurrentPlan("/api/get-current-app", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
 
+  let planName;
+  // Get All Pricing Details with Features
   useEffect(() => {
     if (response.length > 0) {
       dispatch(fetchpricing(response));
     }
   }, [dispatch, response]);
+
+  const freePlan = response?.find((plan) => plan.plan_name === 'Free')
+
+  // Dispatch Active plan data to App Store  
+  useEffect(() => {
+    if (response2) {
+      planName = response2?.name
+      if (Object.keys(response2).length > 0) {
+        const saveCurrentPlan = response?.find((plan) => plan.plan_name === planName);
+        let currentActivePlan = { ...saveCurrentPlan, subscribe_id: response2?.id, status: response2?.status, billing_required: true }
+        dispatch(fetchSavePlan(currentActivePlan));
+      } else {
+        dispatch(fetchSavePlan(freePlan));
+      }
+    }
+  }, [dispatch, response2]);
+
   return (
     <div className="app">
       {activeMenu ? (
