425b79c4a00b75217536012bb129a5b0ae6bc28c
razaakomal <komal.raza@leoatech.com>
2023-05-31 13:37:41 +0500

    XYC-162 Change subscription plan to paid plan

--- web/middleware/subscribe-plan-api.js
+++ web/middleware/subscribe-plan-api.js
@@ -1,18 +1,64 @@
 import { Shopify } from '@shopify/shopify-api';
-import ensureBilling, { BillingInterval } from '../helpers/ensure-billing.js';
+import ensureBilling, { BillingInterval, cancelAppSubscription } from '../helpers/ensure-billing.js';
 import applyAuthMiddleware from './auth.js';
+import NewPool from 'pg';
+import verifyRequest from './verify-request.js';
+const { Pool } = NewPool;
+const pool = new Pool({
+  connectionString: 'postgres://postgres:postgres@localhost:5432/prelauncher',
+});
+
+pool.connect((err, result) => {
+  if (err) throw err;
+  console.log('Connected');
+});
+
 
 export default function SubscribePlanApiEndPoint(myApp) {
-  // POST Request for Subscribing Paid Plan
 
   const BILLING_SETTINGS = {
-    required: false, //initially false ---komal
+    required: false, //initially false 
     // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
     chargeName: 'My Shopify Every Month Charge',
     amount: 0.1,
     currencyCode: 'USD',
     interval: BillingInterval.Every30Days,
   };
+
+
+  // Get the Current Plan Details from Database
+
+  myApp.get('/api/subscribe-plan', async (req, res) => {
+    try {
+      const session = await Shopify.Utils.loadCurrentSession(
+        req,
+        res,
+        myApp.get('use-online-tokens')
+      );
+      console.log(session)
+      try {
+        if (session) {
+          const planExists = await pool.query(
+
+            `select * from subscriptions_list where shop_id =$1`,
+            [session?.shop]
+          );
+
+          console.log(planExists?.rows, "Exists")
+          return res.status(200).json(planExists?.rows);
+        } else {
+          return;
+        }
+      } catch (error) {
+        console.log(error)
+      }
+    } catch (err) {
+      return res.status(500).json(err.message);
+    }
+  });
+
+
+  // POST Request for Subscribing Paid Plan
 
   myApp.post('/api/subscribe-plan', async (req, res) => {
     try {
@@ -22,7 +68,6 @@ export default function SubscribePlanApiEndPoint(myApp) {
         myApp.get('use-online-tokens')
       );
 
-      // console.log(req.body, "Subscribe ")
       const { billing_required, currency_code, plan_name, price } = req.body;
       const plan_settings = {
         ...BILLING_SETTINGS,
@@ -32,8 +77,9 @@ export default function SubscribePlanApiEndPoint(myApp) {
         currencyCode: currency_code,
       };
 
-      console.log('plan', plan_settings);
+      // IF BilLing Required is TRUE & Subscribe Paid Plan
       if (plan_settings.required) {
+
         const [hasPayment, confirmationUrl] = await ensureBilling(
           session,
           plan_settings
@@ -42,16 +88,20 @@ export default function SubscribePlanApiEndPoint(myApp) {
         console.log(confirmationUrl, 'URL');
 
         if (!hasPayment) {
-          console.log(res, 'Response');
+
           return res.json({ confirmationUrl: confirmationUrl });
-          // return res.redirect(`${confirmationUrl}`);
         } else {
           return res.json('You have already subscribed to this plan');
         }
+
+      } else {
+        // Subscribed to Free Plan
+        const result = await cancelAppSubscription(session);
+        return res.status(200).json(result?.appSubscription);
       }
-      applyAuthMiddleware(myApp, { billing: plan_settings });
     } catch (err) {
       console.log(err);
     }
   });
+
 }
