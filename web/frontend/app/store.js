df7508b9bf13d9844affc8d5c27b7088158a3d28
leoatech-Sughra <sughra@leoatech.com>
2023-06-03 14:01:49 +0500

    Update store.js

--- web/frontend/app/store.js
+++ web/frontend/app/store.js
@@ -1,11 +1,10 @@
-
-import { configureStore } from "@reduxjs/toolkit";
-import campaignReducer from "./features/campaigns/campaignSlice";
-import settingsReducer from "./features/settings/settingsSlice";
-import productReducer from "./features/productSlice";
-import userReducer from "./features/users/userSlice";
-import priceReducer from "./features/pricing/pricing";
-import currentPlanReducer from "./features/current_plan/current_plan";
+import { configureStore } from '@reduxjs/toolkit';
+import campaignReducer from './features/campaigns/campaignSlice';
+import settingsReducer from './features/settings/settingsSlice';
+import productReducer from './features/productSlice';
+import userReducer from './features/users/userSlice';
+import priceReducer from './features/pricing/pricing';
+import currentPlanReducer from './features/current_plan/current_plan';
 import referralReducer from './features/referrals/referralSlice';
 
 // All Slices will be added there
@@ -17,7 +16,7 @@ export const store = configureStore({
     products: productReducer,
     users: userReducer,
     pricing: priceReducer,
-    current_plan: currentPlanReducer
+    current_plan: currentPlanReducer,
     referrals: referralReducer,
   },
 });
