d3e8953486196db52505f118d1e0e32781b0bb46
razaakomal <komal.raza@leoatech.com>
2023-06-05 10:47:52 +0500

    XYC-164 Embeded Google Form in Feedback Page

--- web/frontend/components/feedback/Feedback.jsx
+++ web/frontend/components/feedback/Feedback.jsx
@@ -1,8 +1,25 @@
-import React,{useState} from "react";
+import React, { useEffect, useState } from "react";
 import "./feedback.css";
 import StarRating from "./starRating/StarRating";
 const FeedbackComponent = () => {
   const [feedback, setFeedback] = useState("");
+  const [submitted, setSubmitted] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
+  const handleSubmit = () => {
+    // Perform any additional processing or API calls here, if needed
+    // This function will be triggered when the form is submitted
+
+    // Update the state to indicate that the form has been submitted
+    setSubmitted(true)
+  };
+
+
+  useEffect(() => {
+    // Simulating the form loading process
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 500); // Replace this with the actual form loading logic
+  }, []);
   return (
     <div className="feedback-container">
       <div className="feedback-desc">
@@ -14,7 +31,7 @@ const FeedbackComponent = () => {
       <div className="feedback-form">
         <div className="feedback-section-form">
           <h2>Feedback Form</h2>
-          <form>
+          {/* <form>
             <div className="rating-content">
               <div className="link__input">
                 <label htmlFor=""> Store URL</label>
@@ -37,7 +54,13 @@ const FeedbackComponent = () => {
             </div>
 
             <div className="submit-btn">Submit</div>
-          </form>
+          </form> */}
+          {isLoading ? <div class="spinner"></div> : <iframe className="feedback-google-form"
+            src="https://docs.google.com/forms/d/e/1FAIpQLSfN0AGTHf3fczml4OeKvl3RbwC1_rezaa9lH-8krf3MD9Czvw/viewform?embedded=true"
+            frameborder="0"
+            marginheight="0"
+            marginwidth="0"
+          >Loading....</iframe>}
         </div>
       </div>
     </div>
