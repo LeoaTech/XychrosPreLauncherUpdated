1c34b8beacb856022aa2417154462bd465395f62
razaakomal <komal.raza@leoatech.com>
2023-06-05 14:25:47 +0500

    XYC-166 Edit/delete an inactive Campaign data

--- web/frontend/components/campaigns/toggleSwitch/ToggleSwitch.jsx
+++ web/frontend/components/campaigns/toggleSwitch/ToggleSwitch.jsx
@@ -2,13 +2,15 @@ import React, { useState } from "react";
 import "./toggle.css";
 import cx from "classnames";
 
-const ToggleSwitch = ({ rounded, isToggled, onToggle }) => {
+const ToggleSwitch = ({ rounded, isToggled, onToggle, id, start_date, end_date }) => {
   const sliderCX = cx("slider", {
     rounded: rounded,
   });
+  const todayDate = new Date()
   return (
     <label className="switch-label">
-      <input type="checkbox" checked={isToggled} onChange={onToggle} />
+      <input type="checkbox" checked={isToggled}
+      />
       <span className={sliderCX} />
     </label>
   );
