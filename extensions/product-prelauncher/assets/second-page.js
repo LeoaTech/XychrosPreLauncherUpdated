066e2230fb1ba6f96b5c50725e53a0e2289c8919
leoatech-Sughra <sughra@leoatech.com>
2023-06-06 18:25:14 +0500

    updated urls and app details for heroku server

--- extensions/product-prelauncher/assets/second-page.js
+++ extensions/product-prelauncher/assets/second-page.js
@@ -15,17 +15,29 @@ console.log(urlData);
 let referral_inp_field = document.getElementById('code');
 
 // Get Referral Link Copy Button
-let copy_btn = document.getElementById("copy_referral_code_btn");
+let copy_btn = document.getElementById('copy_referral_code_btn');
 
 // Get Socials Icons
 let share_email_referral = document.getElementById('rewards_email_refferal');
-let share_facebook_referral = document.getElementById('rewards_facebook_refferal');
-let share_twitter_referral = document.getElementById('rewards_twitter_refferal');
-let share_snapchat_referral = document.getElementById('rewards_snapchat_refferal');
-let share_instagram_referral = document.getElementById('rewards_instagram_refferal');
+let share_facebook_referral = document.getElementById(
+  'rewards_facebook_refferal'
+);
+let share_twitter_referral = document.getElementById(
+  'rewards_twitter_refferal'
+);
+let share_snapchat_referral = document.getElementById(
+  'rewards_snapchat_refferal'
+);
+let share_instagram_referral = document.getElementById(
+  'rewards_instagram_refferal'
+);
 let share_tiktok_referral = document.getElementById('rewards_tiktok_refferal');
-let share_whatsapp_referral = document.getElementById('rewards_whatsapp_refferal');
-let share_discord_referral = document.getElementById('rewards_discord_refferal');
+let share_whatsapp_referral = document.getElementById(
+  'rewards_whatsapp_refferal'
+);
+let share_discord_referral = document.getElementById(
+  'rewards_discord_refferal'
+);
 
 // Get Referral Count Tagline
 let count_referrals = document.getElementById('count_referrals');
@@ -34,10 +46,14 @@ let count_referrals = document.getElementById('count_referrals');
 let referral_div = document.getElementById('referral_rows');
 
 // Get Current Referrals Container from Timeline Progress
-let referral_count_container_h = document.getElementById('referral-count-achieved') // for horizontal layout
-let referral_count_container_v = document.getElementById('vertical-referral-count-achieved') // for vertical layout
+let referral_count_container_h = document.getElementById(
+  'referral-count-achieved'
+); // for horizontal layout
+let referral_count_container_v = document.getElementById(
+  'vertical-referral-count-achieved'
+); // for vertical layout
 
-// Get Current and Remaining Referral Value Elements from Timeline Progress 
+// Get Current and Remaining Referral Value Elements from Timeline Progress
 let current_referrals = document.getElementById('current_referrals');
 let remaining_referrals = document.getElementById('remaining_referrals');
 
@@ -63,7 +79,7 @@ let tier_discount4 = document.getElementById('discount4');
 const get_referrals = async () => {
   console.log('I came here');
   console.log(campaign_name.innerHTML);
-  const url = '/apps/xychrosupdated/api/get_referrals';
+  const url = '/apps/product-prelauncher/api/get_referrals';
   const response = await fetch(url, {
     method: 'POST',
     headers: {
@@ -78,7 +94,6 @@ const get_referrals = async () => {
   console.log(data);
 
   if (response.status == 200) {
-
     // get campaign data
     let campaign_data = data.campaign_data.rows[0];
     console.log(campaign_data);
@@ -107,14 +122,20 @@ const get_referrals = async () => {
 
     // Share Referral Link via Email
     if (campaign_data.share_email_referral === false) {
-      share_email_referral.style.display = "none";
+      share_email_referral.style.display = 'none';
     } else {
       if (share_email_referral) {
         share_email_referral.addEventListener('click', function (e) {
           e.preventDefault();
           const email_subject = 'Email Subject';
-          const email_body = campaign_data.share_email_message + "\n" + my_referral_link;
-          const mailtoUrl = 'mailto:' + '?subject=' + encodeURIComponent(email_subject) + '&body=' + encodeURIComponent(email_body);
+          const email_body =
+            campaign_data.share_email_message + '\n' + my_referral_link;
+          const mailtoUrl =
+            'mailto:' +
+            '?subject=' +
+            encodeURIComponent(email_subject) +
+            '&body=' +
+            encodeURIComponent(email_body);
           window.location.href = mailtoUrl;
         });
       }
@@ -122,11 +143,13 @@ const get_referrals = async () => {
 
     // Share Referral Link via Facebook
     if (campaign_data.share_facebook_referral === false) {
-      share_facebook_referral.style.display = "none";
+      share_facebook_referral.style.display = 'none';
     } else {
       const shareOnFacebook = function (postText) {
         const facebookUrl = 'https://www.facebook.com/sharer/sharer.php';
-        const fburl = `${facebookUrl}?u=${encodeURIComponent(my_referral_link)}&quote=${encodeURIComponent(postText)}`;
+        const fburl = `${facebookUrl}?u=${encodeURIComponent(
+          my_referral_link
+        )}&quote=${encodeURIComponent(postText)}`;
         window.open(fburl, '_blank', 'width=600,height=400');
       };
       if (share_facebook_referral) {
@@ -140,10 +163,14 @@ const get_referrals = async () => {
 
     // Share Referral Link via Twitter
     if (campaign_data.share_twitter_referral === false) {
-      share_twitter_referral.style.display = "none";
+      share_twitter_referral.style.display = 'none';
     } else {
       function shareOnTwitter(tweetText) {
-        const twitterUrl = 'https://twitter.com/intent/tweet?text=' + encodeURIComponent(tweetText) + '&url=' + encodeURIComponent(my_referral_link);
+        const twitterUrl =
+          'https://twitter.com/intent/tweet?text=' +
+          encodeURIComponent(tweetText) +
+          '&url=' +
+          encodeURIComponent(my_referral_link);
         window.open(twitterUrl, '_blank', 'width=600,height=400');
       }
       if (share_twitter_referral) {
@@ -157,7 +184,7 @@ const get_referrals = async () => {
 
     // Share Referral Link via Snapchat
     if (campaign_data.share_snapchat_referral === false) {
-      share_snapchat_referral.style.display = "none";
+      share_snapchat_referral.style.display = 'none';
     } else {
       function shareOnSnapchat() {
         const snapchatUrl = 'https://www.snapchat.com/';
@@ -166,7 +193,8 @@ const get_referrals = async () => {
       if (share_snapchat_referral) {
         share_snapchat_referral.addEventListener('click', function (e) {
           e.preventDefault();
-          const snapchat_message = campaign_data.share_snapchat_message + "\n" + my_referral_link;
+          const snapchat_message =
+            campaign_data.share_snapchat_message + '\n' + my_referral_link;
           navigator.clipboard.writeText(snapchat_message);
           shareOnSnapchat();
         });
@@ -175,7 +203,7 @@ const get_referrals = async () => {
 
     // Share Referral Link via Instagram
     if (campaign_data.share_instagram_referral === false) {
-      share_instagram_referral.style.display = "none";
+      share_instagram_referral.style.display = 'none';
     } else {
       function shareOnInstagram() {
         const instagramUrl = 'https://www.instagram.com/';
@@ -184,7 +212,8 @@ const get_referrals = async () => {
       if (share_instagram_referral) {
         share_instagram_referral.addEventListener('click', function (e) {
           e.preventDefault();
-          const instagram_message = campaign_data.share_instagram_message + "\n" + my_referral_link;
+          const instagram_message =
+            campaign_data.share_instagram_message + '\n' + my_referral_link;
           navigator.clipboard.writeText(instagram_message);
           shareOnInstagram();
         });
@@ -193,7 +222,7 @@ const get_referrals = async () => {
 
     // Share Referral Link via Tiktok
     if (campaign_data.share_tiktok_referral === false) {
-      share_tiktok_referral.style.display = "none";
+      share_tiktok_referral.style.display = 'none';
     } else {
       function shareOnTiktok() {
         const tiktokUrl = 'https://www.tiktok.com/';
@@ -202,7 +231,8 @@ const get_referrals = async () => {
       if (share_tiktok_referral) {
         share_tiktok_referral.addEventListener('click', function (e) {
           e.preventDefault();
-          const tiktok_message = campaign_data.share_tiktok_message + "\n" + my_referral_link;
+          const tiktok_message =
+            campaign_data.share_tiktok_message + '\n' + my_referral_link;
           navigator.clipboard.writeText(tiktok_message);
           shareOnTiktok();
         });
@@ -211,13 +241,16 @@ const get_referrals = async () => {
 
     // Share Referral Link via Whatsapp
     if (campaign_data.share_whatsapp_referral === false) {
-      share_whatsapp_referral.style.display = "none";
+      share_whatsapp_referral.style.display = 'none';
     } else {
       if (share_whatsapp_referral) {
         share_whatsapp_referral.addEventListener('click', function (e) {
           e.preventDefault();
-          const whatsapp_message = campaign_data.share_whatsapp_message + "\n" + my_referral_link;
-          const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(whatsapp_message)}`;
+          const whatsapp_message =
+            campaign_data.share_whatsapp_message + '\n' + my_referral_link;
+          const whatsappUrl = `https://wa.me/?text=${encodeURIComponent(
+            whatsapp_message
+          )}`;
           window.open(whatsappUrl, '_blank', 'width=600,height=400');
         });
       }
@@ -225,7 +258,7 @@ const get_referrals = async () => {
 
     // Share Referral Link via Discord
     if (campaign_data.share_discord_referral === false) {
-      share_discord_referral.style.display = "none";
+      share_discord_referral.style.display = 'none';
     } else {
       function shareOnDiscord() {
         const discordUrl = 'https://discord.com/channels/@me';
@@ -234,7 +267,8 @@ const get_referrals = async () => {
       if (share_discord_referral) {
         share_discord_referral.addEventListener('click', function (e) {
           e.preventDefault();
-          const discord_message = campaign_data.share_discord_message + "\n" + my_referral_link;
+          const discord_message =
+            campaign_data.share_discord_message + '\n' + my_referral_link;
           navigator.clipboard.writeText(discord_message);
           shareOnDiscord();
         });
@@ -259,7 +293,7 @@ const get_referrals = async () => {
       remaining_referrals.innerText = `${data.referral_data.length}`;
     }
 
-    // set rewards and tiers as per campaign settings 
+    // set rewards and tiers as per campaign settings
     const reward_1_tier = campaign_data.reward_1_tier;
     const reward_2_tier = campaign_data.reward_2_tier;
     const reward_3_tier = campaign_data.reward_3_tier || '';
@@ -279,8 +313,7 @@ const get_referrals = async () => {
       if (reward_4_discount != '') {
         reward_4_discount = `${reward_4_discount}% Off`;
       }
-    }
-    else {
+    } else {
       reward_1_discount = `$${reward_1_discount} Off`;
       reward_2_discount = `$${reward_2_discount} Off`;
       if (reward_3_discount != '') {
@@ -305,11 +338,11 @@ const get_referrals = async () => {
     // if tier 3 and tier 4 are not null:
     if (reward_3_tier != '') {
       if (tier_target3) {
-        tier_target3.style = "";
+        tier_target3.style = '';
         tier_target3.innerHTML = reward_3_tier;
       }
       if (reward_icon3) {
-        reward_icon3.style = "";
+        reward_icon3.style = '';
       }
       if (tier_discount3) {
         tier_discount3.innerHTML = reward_3_discount;
@@ -317,17 +350,16 @@ const get_referrals = async () => {
     }
     if (reward_4_tier != '') {
       if (tier_target4) {
-        tier_target4.style = "";
+        tier_target4.style = '';
         tier_target4.innerHTML = reward_4_tier;
       }
       if (reward_icon4) {
-        reward_icon4.style = "";
+        reward_icon4.style = '';
       }
       if (tier_discount4) {
         tier_discount4.innerHTML = reward_4_discount;
       }
     }
-
   } else {
     console.log(data);
   }
