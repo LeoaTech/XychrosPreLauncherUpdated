c111118a856cd4b3a59c2d89710feb50fa70162f
razaakomal <komal.raza@leoatech.com>
2023-06-16 16:55:20 +0500

    XYC-181 save campaign_details in app store

---
+++ web/frontend/app/features/campaign_details/campaign_details.jsx
@@ -0,0 +1,41 @@
+import { createSlice, current } from "@reduxjs/toolkit";
+
+const initialState = {
+    campaigns_details: [],
+};
+
+export const campaignDetailsSlice = createSlice({
+    name: "campaign_details",
+    initialState: initialState,
+    reducers: {
+
+        fetchCampaignDetails: (state, action) => {
+            state.campaigns_details = action.payload;
+        },
+        updateCampaignDetails: (state, action) => {
+            const { campaign_id } = action.payload;
+            const allCampaigns = state.campaigns_details?.filter(
+                (campaign) => campaign?.campaign_id === action.payload.campaign_id
+            );
+            state.campaigns_details = [...allCampaigns, action.payload];
+        },
+        addNewCampaignDetails: (state, action) => {
+            state.campaigns_details?.push(action.payload);
+        },
+
+    },
+});
+
+// Get All Campaigns
+
+export const fetchCampaignsDetailsList = (state) => state?.campaign_details?.campaigns_details?.filter((camp) => camp.is_deleted === false);
+
+
+// All Action of the campaign
+
+export const {
+    fetchCampaignDetails,
+    updateCampaignDetails,
+    addNewCampaignDetails } = campaignDetailsSlice.actions;
+
+export default campaignDetailsSlice.reducer;
