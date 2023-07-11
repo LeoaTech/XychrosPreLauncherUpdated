6c9d3068e8beb350e3859fe4621ed5f388d1fdc6
razaakomal <komal.raza@leoatech.com>
2023-07-11 11:21:15 +0500

    XYC-206 Change Short summary Card Skelton

--- web/frontend/components/loading_skeletons/SkeletonShortSummaryCard.jsx
+++ web/frontend/components/loading_skeletons/SkeletonShortSummaryCard.jsx
@@ -4,7 +4,7 @@ import React from "react";
 const SkeletonShortSummaryCard = () => {
   return (
     <div className="short-summary-card">
-      <Skeleton classes="values width-100" />
+      <Skeleton classes="values width-50" />
       <Skeleton classes="img width-100" />
     </div>
   );
