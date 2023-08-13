9394be6ff76769e1353346c3d0bb392af3728996
leoatech-Quratulain <quratulain.shoro@leoatech.com>
2023-08-13 14:46:44 +0500

    XYC-137 API to fetc h last six months clicks

--- web/middleware/user_clicks-api.js
+++ web/middleware/user_clicks-api.js
@@ -10,6 +10,7 @@ const pool = new Pool({
 
 pool.connect((err, result) => {
   if (err) throw err;
+  console.log("Connected");
 });
 
 export default function getCampaignClicks(app, secret) {
@@ -17,7 +18,7 @@ export default function getCampaignClicks(app, secret) {
   // count clicks of landing page 
   app.post("/api/getclicks", async (req, res) => {
     try {
-      console.log("I am here in count clicks API");
+      // console.log("I am here in count clicks API");
       // console.log(req.query);
       // console.log(req.body);
       const shop = req.query.shop;
@@ -31,7 +32,7 @@ export default function getCampaignClicks(app, secret) {
 
       if (campaign_data.rowCount > 0) {
         await pool.query(
-          "INSERT INTO clicks(campaign_id, shop) VALUES($1, $2)",
+          "INSERT INTO user_clicks(campaign_id, shop) VALUES($1, $2)",
           [campaign_data.rows[0].campaign_id, shop]
         );
         return res.status(200).json({ success: true, message: "Successully Updated Clicks Table" });
@@ -43,7 +44,7 @@ export default function getCampaignClicks(app, secret) {
     }
   });
 
-  // fetch clicks to be displayed on app frontend
+  // fetch total user clicks of all campaigns 
   app.get("/api/fetchtotalclicks", async (req, res) => {
     try {
       // console.log("I am here in fetch clicks API");
@@ -56,11 +57,35 @@ export default function getCampaignClicks(app, secret) {
       );
       const { shop } = session;
       const total_clicks = await pool.query(
-        "SELECT * FROM clicks WHERE shop=$1", 
+        "SELECT * FROM user_clicks WHERE shop=$1", 
         [shop]
       );
       // console.log('user clicks: ', total_clicks.rowCount);
       return res.status(200).json(total_clicks.rowCount);
+    } catch (error) {
+      return res.status(500).json({ success: false, error: error});
+    }
+  });
+
+// fetch user clicks of last six months of all campaigns 
+  app.get("/api/fetch_lastsixmonths_clicks", async (req, res) => {
+    try {
+      // console.log("I am here in fetch clicks API");
+      // console.log(req.query);
+      // console.log(req.body);
+      const session = await Shopify.Utils.loadCurrentSession(
+        req,
+        res,
+        app.get("use-online-tokens")
+      );
+      const { shop } = session;
+      const lastsixmonths_clicks = await pool.query(
+        "SELECT COUNT(id), to_char(created_at, 'YYYY-MM-DD') AS created_month FROM user_clicks WHERE created_at >= date_trunc('month', CURRENT_DATE) - INTERVAL '6 months' AND created_at < date_trunc('month', CURRENT_DATE) AND shop=$1 GROUP BY created_month ORDER BY created_month ASC;", 
+        [shop]
+      );
+      // console.log('user clicks of last six months: ', lastsixmonths_clicks);
+      // console.log(lastsixmonths_clicks.rows);
+      return res.status(200).json(lastsixmonths_clicks.rows);
     } catch (error) {
       return res.status(500).json({ success: false, error: error});
     }
