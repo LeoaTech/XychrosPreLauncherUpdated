594c4528ce4c683899f8ee3a85910fff3f5703a1
razaakomal <komal.raza@leoatech.com>
2023-09-04 10:28:17 +0500

    xyc-263 highlight pagination button for referral tables

--- web/frontend/components/modal/Modal.jsx
+++ web/frontend/components/modal/Modal.jsx
@@ -1,48 +1,81 @@
-import '../modal/modal.css';
-import DataTable from 'react-data-table-component';
-import { modalColumns, referralRows } from '../referrals/dummyData';
+import "../modal/modal.css";
+import DataTable from "react-data-table-component";
+import { modalColumns, referralRows } from "../referrals/dummyData";
 
 // Data Table custom styles
 const referralsStyles = {
   headCells: {
     style: {
-      fontSize: '15px',
-      fontWeight: 'bold',
+      fontSize: "15px",
+      fontWeight: "bold",
       paddingLeft: "0 8px",
-      justifyContent: 'center',
-      color: '#232227',
-      backgroundColor: '#FCFCFC',
+      justifyContent: "center",
+      color: "#232227",
+      backgroundColor: "#FCFCFC",
 
-      border: '1px solid gray',
-      borderBottom: 'none',
-      borderTop: 'none',
-
-
+      border: "1px solid gray",
+      borderBottom: "none",
+      borderTop: "none",
     },
   },
   cells: {
     style: {
-      textAlign: 'center',
-      alignItems: 'center',
-      justifyContent: 'center',
+      textAlign: "center",
+      alignItems: "center",
+      justifyContent: "center",
       border: " 1px solid gray",
     },
   },
   rows: {
     style: {
-      backgroundColor: '#ECECEC',
-      color: '#232229',
-      textAlign: 'center',
+      backgroundColor: "#ECECEC",
+      color: "#232229",
+      textAlign: "center",
     },
     highlightOnHoverStyle: {
-      color: '#f3f3f3',
-      backgroundColor: 'gray',
-      transitionDuration: '0.15s',
-      transitionProperty: 'background-color',
-      borderBottomColor: 'white',
-      outlineStyle: 'solid',
-      outlineWidth: '1px',
-      outlineColor: 'lightgray',
+      color: "#f3f3f3",
+      backgroundColor: "gray",
+      transitionDuration: "0.15s",
+      transitionProperty: "background-color",
+      borderBottomColor: "white",
+      outlineStyle: "solid",
+      outlineWidth: "1px",
+      outlineColor: "lightgray",
+    },
+  },
+  pagination: {
+    style: {
+      color: "#232229",
+      fontSize: "13px",
+      minHeight: "56px",
+      backgroundColor: "white",
+      border: "1px solid #ccc",
+      borderTop: "none",
+    },
+    pageButtonsStyle: {
+      borderRadius: "50%",
+      height: "40px",
+      width: "40px",
+      padding: "8px",
+      margin: "px",
+      cursor: "pointer",
+      transition: "0.4s",
+      color: "#ccc",
+      fill: "black",
+      backgroundColor: "transparent",
+      "&:disabled": {
+        cursor: "unset",
+        color: "#000",
+        fill: "#000",
+      },
+      "&:hover:not(:disabled)": {
+        backgroundColor: "#000",
+      },
+      "&:focus": {
+        outline: "none",
+        backgroundColor: "#ccc",
+        color:"black"
+      },
     },
   },
 };
@@ -60,13 +93,13 @@ const ShowModal = ({ openModal, setOpenModal, values, fulldata }) => {
   }
 
   return openModal ? (
-    <div className='modal'>
-      <div className='modal_container'>
-        <nav className='modal__nav'>
+    <div className="modal">
+      <div className="modal_container">
+        <nav className="modal__nav">
           <h6>Referral Details</h6>
-          <p className='referral__id'>Campaign: {values?.campaign_name}</p>
+          <p className="referral__id">Campaign: {values?.campaign_name}</p>
         </nav>
-        <section className='modal__body'>
+        <section className="modal__body">
           <p>
             <strong>Email:</strong> {values?.email}
           </p>
@@ -77,27 +110,28 @@ const ShowModal = ({ openModal, setOpenModal, values, fulldata }) => {
             <strong>Friends Joined:</strong> {values?.friends_joined}
           </p>
           <p>
-            <strong>Date of Joining:</strong> {new Date(values?.created_at).toDateString() + " "+ new Date(values?.created_at).toLocaleTimeString()}
+            <strong>Date of Joining:</strong>{" "}
+            {new Date(values?.created_at).toDateString() +
+              " " +
+              new Date(values?.created_at).toLocaleTimeString()}
           </p>
 
           {finalData ? (
-            <div className='referral_detail-table'>
-              <div >
+            <div className="referral_detail-table">
+              <div>
                 <DataTable
                   customStyles={referralsStyles}
                   data={finalData}
                   columns={modalColumns}
                   pagination
+                  highlightOnHover
                 />
               </div>
             </div>
           ) : null}
         </section>
 
-        <button
-          className='btn__close'
-          onClick={handleClose}
-        >
+        <button className="btn__close" onClick={handleClose}>
           Close
         </button>
       </div>
