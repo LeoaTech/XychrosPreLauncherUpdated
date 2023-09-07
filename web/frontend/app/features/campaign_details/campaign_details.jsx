e0f0fe01696fc9779455354ac5980113d38f5824
razaakomal <komal.raza@leoatech.com>
2023-09-07 12:32:36 +0500

    xyc-278 update campaign_details lists global states

--- web/frontend/app/features/campaign_details/campaign_details.jsx
+++ web/frontend/app/features/campaign_details/campaign_details.jsx
@@ -1,26 +1,37 @@
 import { createSlice, current } from "@reduxjs/toolkit";
 
 const initialState = {
-    campaigns_details: [],
+  campaigns_details: [],
 };
 
 export const campaignDetailsSlice = createSlice({
-    name: "campaign_details",
-    initialState: initialState,
-    reducers: {
-        fetchCampaignDetails: (state, action) => {
-            state.campaigns_details = action.payload;
-        }
-
+  name: "campaign_details",
+  initialState: initialState,
+  reducers: {
+    fetchCampaignDetails: (state, action) => {
+      state.campaigns_details = action.payload;
     },
+
+    updateCampaignDetails: (state, action) => {
+      const { campaign_id } = action.payload;
+      const allCampaigns = state.campaigns_details?.filter(
+        (campaign) => campaign?.campaign_id === action.payload.campaign_id
+      );
+      state.campaigns_details = [...allCampaigns, action.payload];
+    },
+  },
 });
 
 // Get All Campaigns
 
-export const fetchCampaignsDetailsList = (state) => state?.campaign_details?.campaigns_details?.filter((camp) => camp?.is_deleted === false);
+export const fetchCampaignsDetailsList = (state) =>
+  state?.campaign_details?.campaigns_details?.filter(
+    (camp) => camp?.is_deleted === false
+  );
 
 // All Action of the campaign
 
-export const {fetchCampaignDetails } = campaignDetailsSlice.actions;
+export const { fetchCampaignDetails, updateCampaignDetails } =
+  campaignDetailsSlice.actions;
 
 export default campaignDetailsSlice.reducer;
