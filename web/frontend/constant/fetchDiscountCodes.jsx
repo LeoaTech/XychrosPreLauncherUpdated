4c90c7feb6ba965b51a6430239dd8ae8e4f7aef1
razaakomal <komal.raza@leoatech.com>
2023-09-22 21:08:58 +0500

    xyc-303 Fetch API to Get All discount codes from Store

---
+++ web/frontend/constant/fetchDiscountCodes.jsx
@@ -0,0 +1,30 @@
+import { useQuery } from 'react-query';
+import { useAuthenticatedFetch } from '../hooks';
+
+const useFetchDiscountCodes = (url) => {
+  const fetchData = useAuthenticatedFetch();
+
+  const fetchDiscountCodes = async () => {
+    const response = await fetchData(url);
+    if (!response.ok) {
+      // throw new Error('Something went wrong while requesting Current Plan');
+    }
+    return response.json();
+  };
+
+  const { data, error } = useQuery('discount-codes', fetchDiscountCodes);
+
+  if (error) {
+    console.log(error);
+    // Handle the error case if needed
+
+    return;
+  }
+
+  return data || []; // Return empty array as default if data is not available yet
+};
+
+export default useFetchDiscountCodes;
+
+
+
