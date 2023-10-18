70ea236b96d9ecf62614ba847b9f5f592efdbec7
razaakomal <komal.raza@leoatech.com>
2023-10-18 17:00:50 +0500

    XYC-327 Save Free Product Reward API

--- web/index.js
+++ web/index.js
@@ -1,41 +1,42 @@
 // @ts-check
-import { join } from 'path';
-import { readFileSync, readFile } from 'fs';
-import express from 'express';
-import cookieParser from 'cookie-parser';
+import { join } from "path";
+import { readFileSync, readFile } from "fs";
+import express from "express";
+import cookieParser from "cookie-parser";
 import {
   Shopify,
   LATEST_API_VERSION,
   ShopifyHeader,
-} from '@shopify/shopify-api';
-import applyAuthMiddleware from './middleware/auth.js';
-import verifyRequest from './middleware/verify-request.js';
-import { setupGDPRWebHooks } from './gdpr.js';
-import redirectToAuth from './helpers/redirect-to-auth.js';
-import { BillingInterval } from './helpers/ensure-billing.js';
-import { AppInstallations } from './app_installations.js';
-import cors from 'cors';
-import campaignApiEndpoints from './middleware/campaign-api.js';
-import referralsApiEndpoints from './middleware/referrals.js';
-import globalSettingsApiEndPoint from './middleware/global-settings-api.js';
-import bodyParser from 'body-parser';
-import createTemplateApiEndpoint from './middleware/create_template.js';
-import templatesApiEndpoints from './middleware/templates_api.js';
-import integrationApi from './middleware/klaviyo-api.js';
-import discountApiEndpoint from './middleware/discount-api.js';
-import userDetailsApiEndPoint from './middleware/userdetails-api.js';
-import getUrlApi from './middleware/geturl-api.js';
-import pricingPlansApiEndpoints from './middleware/get-pricing-plans-api.js';
-import SubscribePlanApiEndPoint from './middleware/subscribe-plan-api.js';
-import campaignDetailsApiEndpoints from './middleware/campaign_details-api.js';
-import getCampaignClicks from './middleware/user_clicks-api.js';
-import deleteFromStoreApiEndpoint from './middleware/delete_from_store-api.js';
-import revenueApiEndpoint from './middleware/revenue-api.js';
-import crypto from 'crypto';
-import { verifyWebhookRequest } from './VerifyWebhook.js';
-import { throwError } from '@shopify/app-bridge/actions/Error/index.js';
-import * as dotenv from 'dotenv';
-import discountCodesApiEndpoints from './middleware/discount-codes-api.js';
+} from "@shopify/shopify-api";
+import applyAuthMiddleware from "./middleware/auth.js";
+import verifyRequest from "./middleware/verify-request.js";
+import { setupGDPRWebHooks } from "./gdpr.js";
+import redirectToAuth from "./helpers/redirect-to-auth.js";
+import { BillingInterval } from "./helpers/ensure-billing.js";
+import { AppInstallations } from "./app_installations.js";
+import cors from "cors";
+import campaignApiEndpoints from "./middleware/campaign-api.js";
+import referralsApiEndpoints from "./middleware/referrals.js";
+import globalSettingsApiEndPoint from "./middleware/global-settings-api.js";
+import bodyParser from "body-parser";
+import createTemplateApiEndpoint from "./middleware/create_template.js";
+import templatesApiEndpoints from "./middleware/templates_api.js";
+import integrationApi from "./middleware/klaviyo-api.js";
+import discountApiEndpoint from "./middleware/discount-api.js";
+import userDetailsApiEndPoint from "./middleware/userdetails-api.js";
+import getUrlApi from "./middleware/geturl-api.js";
+import pricingPlansApiEndpoints from "./middleware/get-pricing-plans-api.js";
+import SubscribePlanApiEndPoint from "./middleware/subscribe-plan-api.js";
+import campaignDetailsApiEndpoints from "./middleware/campaign_details-api.js";
+import getCampaignClicks from "./middleware/user_clicks-api.js";
+import deleteFromStoreApiEndpoint from "./middleware/delete_from_store-api.js";
+import revenueApiEndpoint from "./middleware/revenue-api.js";
+import crypto from "crypto";
+import { verifyWebhookRequest } from "./VerifyWebhook.js";
+import { throwError } from "@shopify/app-bridge/actions/Error/index.js";
+import * as dotenv from "dotenv";
+import discountCodesApiEndpoints from "./middleware/discount-codes-api.js";
+import campaignProductsDetailsApiEndpoints from "./middleware/reward-product-api.js";
 
 dotenv.config();
 
@@ -51,9 +52,9 @@ const DB_PATH = process.env.DATABASE_URL;
 Shopify.Context.initialize({
   API_KEY: process.env.SHOPIFY_API_KEY,
   API_SECRET_KEY: process.env.SHOPIFY_API_SECRET,
-  SCOPES: process.env.SCOPES.split(','),
-  HOST_NAME: process.env.HOST.replace(/https?:\/\//, ''),
-  HOST_SCHEME: process.env.HOST.split('://')[0],
+  SCOPES: process.env.SCOPES.split(","),
+  HOST_NAME: process.env.HOST.replace(/https?:\/\//, ""),
+  HOST_SCHEME: process.env.HOST.split("://")[0],
   API_VERSION: LATEST_API_VERSION,
   IS_EMBEDDED_APP: true,
   // This should be replaced with your preferred storage strategy
@@ -66,9 +67,9 @@ Shopify.Context.initialize({
 const BILLING_SETTINGS = {
   required: false, //initially false
   // This is an example configuration that would do a one-time charge for $5 (only USD is currently supported)
-  chargeName: 'My Shopify Every Month Charge',
+  chargeName: "My Shopify Every Month Charge",
   amount: 0.1,
-  currencyCode: 'USD',
+  currencyCode: "USD",
   interval: BillingInterval.Every30Days,
 };
 
@@ -78,17 +79,17 @@ const BILLING_SETTINGS = {
 //
 // More details can be found on shopify.dev:
 // https://shopify.dev/apps/webhooks/configuration/mandatory-webhooks
-setupGDPRWebHooks('/api/webhooks');
+setupGDPRWebHooks("/api/webhooks");
 
 // export for test use only
 export async function createServer(
   root = process.cwd(),
-  isProd = process.env.NODE_ENV == 'production',
+  isProd = process.env.NODE_ENV == "production",
   billingSettings = BILLING_SETTINGS
 ) {
   const app = express();
 
-  app.set('use-online-tokens', USE_ONLINE_TOKENS);
+  app.set("use-online-tokens", USE_ONLINE_TOKENS);
   app.use(cookieParser(Shopify.Context.API_SECRET_KEY));
 
   applyAuthMiddleware(app, {
@@ -100,7 +101,7 @@ export async function createServer(
   // See https://github.com/Shopify/shopify-api-node/blob/main/docs/usage/webhooks.md#note-regarding-use-of-body-parsers
   // for more details.
 
-  app.post('/api/webhooks', async (req, res) => {
+  app.post("/api/webhooks", async (req, res) => {
     try {
       await Shopify.Webhooks.Registry.process(req, res);
       console.log(`Webhook processed, returned status code 200`);
@@ -115,14 +116,14 @@ export async function createServer(
   });
 
   // Add middlewares to the request on '/api/webhooks' endpoint
-  app.use('/api/webhooks', verifyWebhookRequest, setupGDPRWebHooks);
+  app.use("/api/webhooks", verifyWebhookRequest, setupGDPRWebHooks);
 
   //  API to get All Products in my Shopify Store
   app.get(`/api/2022-10/products.json`, async (req, res) => {
     const session = await Shopify.Utils.loadCurrentSession(
       req,
       res,
-      app.get('use-online-tokens')
+      app.get("use-online-tokens")
     );
 
     const { Product } = await import(
@@ -154,7 +155,7 @@ export async function createServer(
 
   // All endpoints after this point will require an active session
   app.use(
-    '/api/*',
+    "/api/*",
     verifyRequest(app, {
       billing: billingSettings,
     })
@@ -162,6 +163,7 @@ export async function createServer(
 
   campaignApiEndpoints(app);
   campaignDetailsApiEndpoints(app);
+  campaignProductsDetailsApiEndpoints(app);
   referralsApiEndpoints(app);
   createTemplateApiEndpoint(app);
   globalSettingsApiEndPoint(app);
@@ -177,32 +179,32 @@ export async function createServer(
     const shop = Shopify.Utils.sanitizeShop(req.query.shop);
     if (Shopify.Context.IS_EMBEDDED_APP && shop) {
       res.setHeader(
-        'Content-Security-Policy',
+        "Content-Security-Policy",
         `frame-ancestors https://${encodeURIComponent(
           shop
         )} https://admin.shopify.com;`
       );
     } else {
-      res.setHeader('Content-Security-Policy', `frame-ancestors 'none';`);
+      res.setHeader("Content-Security-Policy", `frame-ancestors 'none';`);
     }
     next();
   });
 
   if (isProd) {
-    const compression = await import('compression').then(
+    const compression = await import("compression").then(
       ({ default: fn }) => fn
     );
-    const serveStatic = await import('serve-static').then(
+    const serveStatic = await import("serve-static").then(
       ({ default: fn }) => fn
     );
     app.use(compression());
     app.use(serveStatic(PROD_INDEX_PATH, { index: false }));
   }
 
-  app.use('/*', async (req, res, next) => {
-    if (typeof req.query.shop !== 'string') {
+  app.use("/*", async (req, res, next) => {
+    if (typeof req.query.shop !== "string") {
       res.status(500);
-      return res.send('No shop provided');
+      return res.send("No shop provided");
     }
 
     const shop = Shopify.Utils.sanitizeShop(req.query.shop);
@@ -212,7 +214,7 @@ export async function createServer(
       return redirectToAuth(req, res, app);
     }
 
-    if (Shopify.Context.IS_EMBEDDED_APP && req.query.embedded !== '1') {
+    if (Shopify.Context.IS_EMBEDDED_APP && req.query.embedded !== "1") {
       const embeddedUrl = Shopify.Utils.getEmbeddedAppUrl(req);
 
       return res.redirect(embeddedUrl + req.path);
@@ -220,12 +222,12 @@ export async function createServer(
 
     const htmlFile = join(
       isProd ? PROD_INDEX_PATH : DEV_INDEX_PATH,
-      'index.html'
+      "index.html"
     );
 
     return res
       .status(200)
-      .set('Content-Type', 'text/html')
+      .set("Content-Type", "text/html")
       .send(readFileSync(htmlFile));
   });
 
