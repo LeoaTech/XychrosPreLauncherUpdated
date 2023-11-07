98044b8377d7058265d4f185a6a2e23200283008
razaakomal <komal.raza@leoatech.com>
2023-11-07 12:07:20 +0500

    xyc-346 Update Light Theme colors for Charts

--- web/frontend/components/ui/Charts.jsx
+++ web/frontend/components/ui/Charts.jsx
@@ -1,30 +1,44 @@
-import './Charts.css';
-import LineChart from './LineChart';
-import DonutChart from './DonutChart';
-import RadarChart from './RadarChart';
+import "./Charts.css";
+import LineChart from "./LineChart";
+import DonutChart from "./DonutChart";
+import RadarChart from "./RadarChart";
+import { useThemeContext } from "../../contexts/ThemeContext";
 
 export default function Charts(props) {
+  const { theme } = useThemeContext();
   const chartType = props.type;
   return (
-    <div id={props.type} className='chart'>
-      <div className='chart-header'>
-        <div className='chart-heading'>{props.header}</div>
-        <div className='chart-value'>{props.value}</div>
-        <div className='chart-subheading'>{props.subheader}</div>
+    <div id={props.type} className={theme === "dark" ? "chart" : "chart-light"}>
+      <div className={theme === "dark" ? "chart-header" : "chart-header-light"}>
+        <div
+          className={theme === "dark" ? "chart-heading" : "chart-heading-light"}
+        >
+          {props.header}
+        </div>
+        <div className={theme === "dark" ? "chart-value" : "chart-value-light"}>
+          {props.value}
+        </div>
+        <div
+          className={
+            theme === "dark" ? "chart-subheading" : "chart-subheading-light"
+          }
+        >
+          {props.subheader}
+        </div>
       </div>
-      {chartType == 'line' && (
+      {chartType == "line" && (
         <LineChart
           options={props.LineChartOptions}
           data={props.LineChartData}
         />
       )}
-      {chartType == 'radar' && (
+      {chartType == "radar" && (
         <RadarChart
           options={props.RadarChartOptions}
           data={props.RadarChartData}
         />
       )}
-      {chartType == 'donut' && (
+      {chartType == "donut" && (
         <DonutChart
           options={props.DonutChartOptions}
           data={props.DonutChartData}
