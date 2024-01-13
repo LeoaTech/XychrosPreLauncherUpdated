4110c159795f01b6b9fdb9092b104d0506846753
razaakomal <komal.raza@leoatech.com>
2024-01-13 07:19:14 +0500

    xyc-345 Update Feedback Form and Page for Theme Mode

--- web/frontend/pages/feedback.jsx
+++ web/frontend/pages/feedback.jsx
@@ -3,54 +3,37 @@ import { SideBar, Header, MainPage } from "../components/index";
 import { useStateContext } from "../contexts/ContextProvider";
 import { useThemeContext } from "../contexts/ThemeContext";
 import "../index.css";
-
+import SkeletonLoader from "../components/loading_skeletons/SkeletonTable";
 
 // Code Splitting
 const Feedback = lazy(() => import("../components/feedback/Feedback"));
 
-
 const FeedbackPage = () => {
-  const { activeMenu } = useStateContext();
-  const { darkTheme } = useThemeContext();
+  const { darkTheme, theme } = useThemeContext();
   // Page render Scroll to Top
   useEffect(() => {
     window.scrollTo(0, 0);
-  }, [])
+  }, []);
   return (
-    <div className="app">
-      {activeMenu ? (
-        <div className="header">
-          <Header />
+    <div className={theme === "dark" ? "app" : "app-light"}>
+
+
+      <input type="checkbox" name="" id="menu-toggle" />
+      <div className="overlay">
+        <label htmlFor="menu-toggle"> </label>
+      </div>
+      <div className="sidebar">
+        <div className="sidebar-container">
+          <SideBar />
         </div>
-      ) : (
-        <div className="header">
-          <Header />
-        </div>
-      )}
-      <div className="main-app">
-        {activeMenu ? (
-          <>
-            <div className="sidebar">
-              <SideBar />
-            </div>
-            <div className="main-container">
-              <Suspense fallback={<div>Loading...</div>}>
-                <Feedback />
-              </Suspense>
-            </div>
-          </>
-        ) : (
-          <>
-            <div className="sidebar closed">
-              <SideBar />
-            </div>
-            <div className="main-container full">
-              <Suspense fallback={<div>Loading...</div>}>
-                <Feedback />
-              </Suspense>
-            </div>
-          </>
-        )}
+      </div>
+      <div className="main-content">
+        <Header />
+        <main>
+          <Suspense fallback={<SkeletonLoader />}>
+            <Feedback />
+          </Suspense>
+        </main>
       </div>
     </div>
   );
